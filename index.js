5452158dd3954b6a855b6aa0686f57ffbaef69b4
dashing-page-6640 <elhadi.sulton@gmail.com>
2018-09-16 18:51:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,13 @@
      katzDeli.push(newCustomer);
      return ('Welcome, ' + newCustomer + '. You are number ' + katzDeli.length + ' in line.')
   }
+
+ function nowServing(katzDeliLine){
+   if (katzDeliLine === 0){
+     return "There is nobody waiting to be served!";
+   }
+
+   var customer = katzDeliLine.shift();
+   return "Currently serving " + customer + ".";
+
+ }
