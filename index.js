18f04f13ad849c724e0af2b5ed39e566df71d5e8
dashing-page-6640 <elhadi.sulton@gmail.com>
2018-09-16 18:43:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
   function takeANumber(katzDeli, newCustomer) {
 
-    if (katzDeli.length > 0)
+    if (katzDeli.length > 0);
      katzDeli.push(newCustomer);
      return ('Welcome, ' + newCustomer + '. You are number ' + katzDeli.length + ' in line.')
   }
